3071e166e0096f4ba1817a0da777ccfa7c31719b
8287782435 <iamruhil77@gmail.com>
2016-02-19 22:18:55 +0530

    Added files via upload

---
+++ merge.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i=0,j=n-1,n,a,k;
+    do{
+        k=(i+j)/2;
+        if(a[k]<=x)
+            i=k+1;
+        else
+            j=k-1;
+        }
+while(i<=j);
+{
+    printf("%d result",i);
+}
+}
